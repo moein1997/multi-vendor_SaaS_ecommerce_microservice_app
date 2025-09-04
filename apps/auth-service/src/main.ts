6cf2d079a5fcf8d26e797b16583e8590c3f62710
Moein-mac <moein.azizi1997@gmail.com>
2025-09-05 01:10:04 +0330

    configuring auth-server for error handling

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,30 @@
 import express from 'express';
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 6001;
+import cors from 'cors';
+
+
+
+// const host = process.env.HOST ?? 'localhost';
 
 const app = express();
+
+app.use(cors({
+    origin : ['http://localhost:3000'],
+    allowedHeaders :['Content-Type', 'Authorization'],
+    credentials : true
+  }
+));
+
+const port = process.env.PORT ? Number(process.env.PORT) : 6001;
 
 app.get('/', (req, res) => {
     res.send({ 'message': 'Hello API'});
 });
 
-app.listen(port, host, () => {
-    console.log(`[ ready ] http://${host}:${port}`);
+const server = app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}/api`);
+});
+
+server.on('error', (error) => {
+    console.error("Server Error:  ",error);
 });
